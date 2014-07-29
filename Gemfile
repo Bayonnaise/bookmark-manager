1b610686216e3c8b717704225cc0de945bcf45a3
David Bayon <dbayon82@googlemail.com>
2014-07-29 16:30:54 +0100

    Testing email

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'bcrypt-ruby'
 gem 'rack-flash3'
 gem 'sinatra-partial'
 gem 'pg'
+gem 'rest_client'
 
 group :development, :test do
   gem 'capybara'
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,31 +1,44 @@
-	get '/users/new' do
-	  @user = User.new
-		erb :"users/new"
-	end
+get '/users/new' do
+  @user = User.new
+	erb :"users/new"
+end
 
-	post '/users' do
-		@user = User.create(:email => params[:email], :password => params[:password], 
-	    :password_confirmation => params[:password_confirmation])
-	  if @user.save
-	  	session[:user_id] = @user.id
-	  	redirect to('/')
-	  else
-	    flash.now[:errors] = @user.errors.full_messages
-	    erb :"users/new"
-	  end
-	end
+post '/users' do
+	@user = User.create(:email => params[:email], :password => params[:password], 
+    :password_confirmation => params[:password_confirmation])
+  if @user.save
+  	session[:user_id] = @user.id
+  	redirect to('/')
+  else
+    flash.now[:errors] = @user.errors.full_messages
+    erb :"users/new"
+  end
+end
 
-	# post '/users/reset_password/' do
-	# 	email = params[:email]
-	# 	user = User.first(:email => email)
-	# 	user.password_token = (1..64).map{('A'..'Z').to_a.sample}.join
-	# 	user.password_token_timestamp = Time.now
-	# 	user.save
+post '/users/reset_password/' do
+	email = params[:email]
+	user = User.first(:email => email)
+	user.password_token = (1..64).map{('A'..'Z').to_a.sample}.join
+	user.password_token_timestamp = Time.now
+	user.save
+	send_email(user)
+end
 
-	# 	# <a href="localhost:9292/users/reset_password/#{token}">Reset password</a>
-	# 	# erb :"users/reset_password/:token"
-	# end
+get '/users/reset_password/:token' do
+	user = User.first(:password_token => token)
 
-	# get '/users/reset_password/:token' do
-	# 	user = User.first(:password_token => token)
-	# end
+
+end
+
+API_KEY = ENV['MAILGUN_API_KEY']
+API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/postmaster@app27923148.mailgun.org"
+
+def send_email(user)
+  RestClient.post API_URL+"/messages"
+
+  :from => "Dave and Michiel <me@samples.mailgun.org>",
+  :to => user.email,
+  :subject => "Password reset",
+  :text => <a href="localhost:9292/users/reset_password/#{user.password_token}">Click here to reset your password.</a>
+end
+
