9cdfcb4c706325ed0c880542e116ddac669a8948
David Bayon <dbayon82@googlemail.com>
2014-07-30 10:44:26 +0100

    Testing email

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -18,7 +18,7 @@ end
 post '/users/reset_password' do
 	email = params[:email]
 	user = User.first(:email => email)
-	password_token = (1..49).map{('A'..'Z').to_a.sample}.join
+	password_token = (1..64).map{('A'..'Z').to_a.sample}.join
 	password_token_timestamp = Time.now
 	user.update(:password_token => password_token, :password_token_timestamp => password_token_timestamp)
 
@@ -29,14 +29,14 @@ end
 
 get '/users/reset_password/:token' do
 	user = User.first(:password_token => token)
+	erb :reset_password
 end
 
 API_KEY = ENV['MAILGUN_API_KEY']
 API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/postmaster@app27923148.mailgun.org"
 
 def send_email(user)
-	link = "localhost:9292/users/reset_password/#{user.password_token}"
-
+	link = "http://radiant-refuge-8401.herokuapp.com/users/reset_password/#{user.password_token}"
   RestClient.post "https://api:#{API_KEY}"\
   "@api.mailgun.net/v2/app27923148.mailgun.org/messages",
   :from => "Dave and Michiel <me@app27923148.mailgun.org>",
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User
 	property :id, Serial
 	property :email, String, :unique => true, :message => "This email is already taken"
 	property :password_digest, Text
-	property :password_token, String
+	property :password_token, String, :length => 64 
 	property :password_token_timestamp, String
 
 	def password=(password)
