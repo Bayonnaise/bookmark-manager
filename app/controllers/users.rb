624b557bc13058ca7024bcfe9902a0e93dd231d4
David Bayon <dbayon82@googlemail.com>
2014-07-30 11:46:28 +0100

    bug fixes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -37,7 +37,8 @@ API_KEY = ENV['MAILGUN_API_KEY']
 API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/postmaster@app27923148.mailgun.org"
 
 def send_email(user)
-	link = "http://radiant-refuge-8401.herokuapp.com/users/reset_password/#{user.password_token}"
+	# link = "http://radiant-refuge-8401.herokuapp.com/users/reset_password/#{user.password_token}"
+	link = "localhost:9292/users/reset_password/#{user.password_token}"
   RestClient.post "https://api:#{API_KEY}"\
   "@api.mailgun.net/v2/app27923148.mailgun.org/messages",
   :from => "Dave and Michiel <me@app27923148.mailgun.org>",
